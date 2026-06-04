9b4178c70252f835af8025adb0ab11d5a022153b
Nalin Vaidya <nalin.vaidya@midasteknologi.com>
2026-06-04 19:25:54 +0530

    feat(docker): changed configuration while creating docker image

--- config/api.js
+++ config/api.js
@@ -1,4 +1,4 @@
 
-export const API_BASE_URL = '/api';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export const AUTH_STORAGE_KEY = 'ekyc_access_token';
 export const USER_STORAGE_KEY = 'ekyc_user';
